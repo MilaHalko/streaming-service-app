3633f990a66fb3b93964fb0512497898f18a3792
milka_deluxe <milagalko1@gmail.com>
2023-12-13 13:55:39 +0200

    images, genres, movies data + links

---
+++ data/genres.js
@@ -0,0 +1,118 @@
+export const Genres = [
+    {
+        id: '1',
+        genre: 'Action'
+    },
+    {
+        id: '2',
+        genre: 'Adventure'
+    },
+    {
+        id: '3',
+        genre: 'Animation'
+    },
+    {
+        id: '4',
+        genre: 'Biography'
+    },
+    {
+        id: '5',
+        genre: 'Children'
+    },
+    {
+        id: '6',
+        genre: 'Comedy'
+    },
+    {
+        id: '7',
+        genre: 'Crime'
+    },
+    {
+        id: '8',
+        genre: 'Documentary'
+    },
+    {
+        id: '9',
+        genre: 'Drama'
+    },
+    {
+        id: '10',
+        genre: 'Family'
+    },
+    {
+        id: '11',
+        genre: 'Fantasy'
+    },
+    {
+        id: '12',
+        genre: 'Film Noir'
+    },
+    {
+        id: '13',
+        genre: 'History'
+    },
+    {
+        id: '14',
+        genre: 'Horror'
+    },
+    {
+        id: '15',
+        genre: 'Music'
+    },
+    {
+        id: '16',
+        genre: 'Musical'
+    },
+    {
+        id: '17',
+        genre: 'Mystery'
+    },
+    {
+        id: '18',
+        genre: 'Narrative'
+    },
+    {
+        id: '19',
+        genre: 'Romance'
+    },
+    {
+        id: '20',
+        genre: 'Sci-Fi'
+    },
+    {
+        id: '21',
+        genre: 'Sport'
+    },
+    {
+        id: '22',
+        genre: 'Spy'
+    },
+    {
+        id: '23',
+        genre: 'Superhero'
+    },
+    {
+        id: '24',
+        genre: 'Thriller'
+    },
+    {
+        id: '25',
+        genre: 'War'
+    },
+    {
+        id: '26',
+        genre: 'Western'
+    },
+    {
+        id: '27',
+        genre: 'Mafia'
+    },
+    {
+        id: '28',
+        genre: 'Anime'
+    },
+    {
+        id: '29',
+        genre: 'Adaptation'
+    }
+];
