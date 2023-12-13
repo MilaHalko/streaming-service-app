40bd570d5ca36139d525ca683c64b577c915c1b6
milka_deluxe <milagalko1@gmail.com>
2023-12-13 14:32:03 +0200

    yarn add react-router-dom, swiper, aos + google fonts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 function App() {
     return (
         <div className="h-header bg-main w-full">
-        <h1 className="text-h1 text-star font-bold underline">Hello world!</h1>
+            <h1 className="text-h1 text-star font-bold underline">Hello world!</h1>
+            <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
