74ad3478b427a3545f3bb521d72ffda33d99d3b2
milka_deluxe <milagalko1@gmail.com>
2023-12-14 03:12:51 +0200

    init: components & routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen";
 import AboutUs from "./Screens/AboutUs";
+import ContactUs from "./Screens/ContactUs";
+import Movies from "./Screens/Movies";
 import NotFound from "./Screens/NotFound";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
         <Routes>
             <Route path="/" element={<HomeScreen />} />
             <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/movies" element={<Movies />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
