01cdfdf5f40ce282f2e5e44b8c15c652ffa0aa0d
milka_deluxe <milagalko1@gmail.com>
2023-12-20 01:44:14 +0200

    new: Login & Signin & NotFound screens

--- src/Screens/NotFound.js
+++ src/Screens/NotFound.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+import {BiHomeAlt} from "react-icons/bi";
 
 function NotFound() {
     return (
-        <div>NotFound</div>
+        <div className='flex-colo gap-8 w-full min-h-screen text-white bg-main lg:py-20 py-10 px-6'>
+            <h1 className='text-4xl font-semibold'>Page Not Found :(</h1>
+            <p className='font-medium text-border italic leading-6'>
+                The page you are looking for
+                might have been removed had its name changed
+                or is temporarily unavailable.
+            </p>
+            <Link to="/" className='bg-subMain transitions text-white flex-rows gap-2 font-medium py-3 px-4 rounded-md hover:text-main'>
+                <BiHomeAlt/> Mediatoria
+            </Link>
+        </div>
     )
 }
 
