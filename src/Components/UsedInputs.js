ba43ea7c956170cd55da8c4e5a3f6ff884fa1409
milka_deluxe <milagalko1@gmail.com>
2023-12-20 01:41:45 +0200

    new: multiple select, input components

--- src/Components/UsedInputs.js
+++ src/Components/UsedInputs.js
@@ -8,13 +8,25 @@ export const Message = ({label, placeholder}) => {
     )
 };
 
-export const Select = ({label, options, onChange}) => {
+export const Select = ({label, options, onChange, multiple = false}) => {
     return (
         <>
         <label className="text-border font-semibold">{label}</label>
-        <select className="w-full mt-2 px-6 py-4 text-text bg-main border border-border rounded" onChange={onChange}>
+        <select className="w-full mt-2 px-6 py-4 text-text bg-main border border-border rounded" multiple={multiple} onChange={onChange}>
             {options.map((o, index) => (<option key={index} value={o.value}>{o.title}</option>))}
         </select>
         </>
+    )
+};
+
+export const Input = ({label, placeholder, type, bg}) => {
+    return (
+        <div className="text-sm w-full">
+            <label className="text-border font-semibold">{label}</label>
+            <input required
+                   type={type}
+                   placeholder={placeholder}
+                   className={`w-full text-sm mt-2 p-3 border border-border rounded text-white ${bg ? 'bg-main' : 'bg-dry'}`}/>
+        </div>
     )
 };
