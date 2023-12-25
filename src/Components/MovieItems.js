54356b7459943bc13bc43b72ea36431073e37e5f
milka_deluxe <milagalko1@gmail.com>
2023-12-25 16:59:35 +0200

    delete: images

---
+++ src/Components/MovieItems.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {GetMovieGenres} from "./Functions/GetMovieGenres";
+import {FaRegCalendarAlt} from "react-icons/fa";
+
+function FlexMovieItems({movie}) {
+    const genres = GetMovieGenres(movie)
+    return (
+        <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+                {genres.map((genre, index) => (
+                    <div key={index}>
+                        <h3 className="text-sm font-medium truncate">{genre}</h3>
+                        {index < genres.length - 1 && <span key={index} className="text-sm font-medium">/</span>}
+                    </div>
+                ))}
+            </div>
+            <div className="flex items-center gap-2">
+                <FaRegCalendarAlt className="text-subMain w-3 h-3"/>
+                <span className="text-sm font-medium">{movie.release_date}</span>
+            </div>
+        </div>
+    )
+}
+
+export default FlexMovieItems
--- src/Screens/Dashboard/FavoriteMovies.js
+++ src/Screens/Dashboard/FavoriteMovies.js
@@ -7,13 +7,13 @@ import {db} from "../../firebase";
 
 function FavouriteMovies() {
     const {user} = UserAuth()
-    const [FavoriteMovies, setFavoriteMovies] = React.useState([])
+    const [FavouriteMovies, setFavouriteMovies] = React.useState([])
     const [admin, setAdmin] = React.useState(false)
 
     React.useEffect(() => {
         if (user?.email) {
             const unsubscribe = onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
-                setFavoriteMovies(doc.data()?.favoriteMovies)
+                setFavouriteMovies(doc.data()?.favoriteMovies)
                 setAdmin(doc.data()?.role === 'admin')
             });
 
@@ -24,14 +24,9 @@ function FavouriteMovies() {
 
     return (
         <SideBar>
-            <div className="flex flex-col gap-6">
-                <div className="flex-btn gap-2">
-                    <h2 className="text-xl font-bold">Favorite Movies</h2>
-                    <button className="bg-main font-medium transitions hover:bg-subMain border border-subMain text-white py-3 px-6 rounded">
-                        Delete All
-                    </button>
-                </div>
-                <Table movies={FavoriteMovies} admin={admin}/>
+            <div className="flex flex-col gap-5">
+                <h2 className="text-xl font-bold">Favourite Movies</h2>
+                <Table movies={FavouriteMovies} admin={admin}/>
             </div>
         </SideBar>
     )
